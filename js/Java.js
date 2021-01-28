5f9cfd22b9d2b69af56950ff0037358e2e85013a
unknown <d1360ms@gmail.com>
2021-01-28 11:56:48 -0500

    se agrega javascript a la calculadora

--- js/Java.js
+++ js/Java.js
@@ -1,2 +1,52 @@
 //alert("Bienvenidos");
 //swal("Bienvenidos");
+
+function updateinput(newValue, clear=false) {
+    var div= document.getElementById("total");
+
+    div.value = clear ? newValue : `${div.value}${newValue}`;
+}
+
+function imprimiropera(data) {
+    updateinput(` ${data.value} `);
+}
+
+function imprimirnumero(data) {
+    updateinput(data.value);
+}
+
+function limpiarinput() {
+    updateinput("", true);
+}
+
+function igualar() {
+    var data = document.getElementById("total").value;
+    var array = validaoper(data.split(" "));
+    updateinput(array, true);
+
+    alert(data.split(" "));
+}
+
+function validaoper(arrayData) {
+    var resultado;
+    var numero1, numero2;
+
+    numero1 = Number(arrayData[0]);
+    numero2 = Number(arrayData[2]);
+
+    switch (arraydata[1]) {
+        case '+':
+            resultado = numero1 + numero2;
+            break;
+        case '-':
+            resultado = numero1 - numero2;
+            break;
+        case '*':
+            resultado = numero1 * numero2;
+            break;
+        case '/':
+            resultado = numero1 / numero2;
+            break;
+    }
+    return resultado;
+}
